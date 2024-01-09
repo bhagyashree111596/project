442e106fbff010b59a944232fa1596cab8b9a45e
pbhagyashree94 <pbhagayshree94@gmail.com>
2024-01-09 21:57:22 +0530

    new file

---
+++ src/main/java/com/example/controller/HotelController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hotel")
+public class HotelController {
+   @GetMapping
+   public String getAll() {
+	   return "welcome to project";
+   }
+}
